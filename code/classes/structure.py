a78a28567b05f2fb9c1a0abdf7cd51086706cad3
Liz Mooij <lizmooij@wcw-stud-145-109-25-99.wireless.uva.nl>
2020-01-09 12:28:31 +0100

    update en random_move gemaakt, nog een paar foutmeldingen

--- code/classes/structure.py
+++ code/classes/structure.py
@@ -1,4 +1,4 @@
-import csv, os, cs50
+import csv, os, random
 
 class Game():
     """
@@ -51,7 +51,7 @@ class Game():
         """
             Returns True if the game is won, otherwise false.
         """
-        if self.redcar.x == gridsize - 2 && self.redcar.y == self.gridexit:
+        if self.redcar.x == gridsize - 2 and self.redcar.y == self.gridexit:
             return True
         else:
             return False
@@ -64,65 +64,95 @@ class Game():
 
         return self.grid
 
-        #
-        # def manual(self):
-        #     sd
-
-
-    def random(self):
-
-        car = random.choice(self.cars)
-        carlist = []
-        carlist.append(car)
-        print(carlist)
-        print("hoi")
-        return carlist
-
-    def update(self, game, car, x, y):
+    def update(self, car, x, y):
         """
             Updates the coordinates of a car and the grid.
         """
-            car.x = x
-            car.y = y
-            for i in range(game.gridsize):
-                for j in range(game.gridsize):
-                    if grid[i][j] == car.id:
-                        grid[i][j] = 0
-            temporary_x = car.x - 1
-            temporary_y = car.y - 1
-            if car.orientation == "V":
-                for i in range(car.length):
-                    self.grid[x][y] = car.id
-                    y += 1
-            else:
-                for i in range(car.length):
-                    self.grid[x][y] = car.id
-                    x += 1
 
-    def move(self, car, direction):
-        """
-        de input hier is een keuze in auto en een keuze van - of +
-        """
+        car.x = x
+        car.y = y
+        for i in range(self.gridsize):
+            for j in range(self.gridsize):
+                if grid[i][j] == car.id:
+                    grid[i][j] = 0
+        temporary_x = car.x - 1
+        temporary_y = car.y - 1
+        if car.orientation == "V":
+            for i in range(car.length):
+                self.grid[x][y] = car.id
+                y += 1
+        else:
+            for i in range(car.length):
+                self.grid[x][y] = car.id
+                x += 1
 
-        """
-        For moving the board, check Orientation,
-        If Orientation is H only alteration of X is allowed, ergo only Y alteration is allowed if orientation is V
+    def random_move(self):
 
-        - Start with steps of 1.
+        car_possible = False
+        while not car_possible:
+            car = random.choice(self.cars)
+            move_y_positive = False
+            move_y_negative = False
+            move_x_positive = False
+            move_x_negative = False
 
-        - Prompt user for input (find the number game)
-        - ask which car they want to move (A/Z)
-        - Direction? (- or +)
-        - All grids with identity of car, check if block next to them is either 0 or the same identity.
-        - If not cancel move and return INVALID MOVE!
-        - otherwise change the grid
-        - change the coordinates in the item car.
-        -random is basically, a loop that just selects a car at random,
-        selects movement at random
-        TADAAA
-        - Next step is if the road to the exit is empty finish the game!
-        """
+            #onderstaande code geeft nog een foutmelding
 
+            if car.orientation == 'V':
+                if self.grid[car.x][car.y+car.length]:
+                    if self.grid[car.x][car.y+car.length] == 0:
+                        move_y_positive = True
+                if self.grid[car.x][car.y-1]:
+                    if self.grid[car.x][car.y-1] == 0:
+                        move_y_negative = True
+
+            if car.orientation == 'H':
+                if self.grid[car.x+car.length][car.y]:
+                    if self.grid[car.x+car.length][car.y] == 0:
+                        move_x_positive = True
+                if self.grid[car.x-1][car.y]:
+                    if self.grid[car.x-1][car.y] == 0:
+                        move_x_negative = True
+
+            if move_y_positive or move_y_negative or move_x_positive or move_x_negative:
+                car_possible = True
+
+        if move_y_positive or move_y_negative:
+            x = car.x
+
+            if move_y_positive and move_y_negative:
+                random_choice =  random.choice([0, 1])
+                if random_choice == 1:
+                    move_y_positive = False
+                else:
+                    move_y_negative = False
+
+            if move_y_positive:
+                y = car.y + car.length
+                self.update(car, x, y)
+
+            elif move_y_negative:
+                y = car.y - 1
+                self.update(car, x, y)
+
+        if move_x_positive or move_x_negative:
+            y = car.y
+
+            if move_x_positive and move_x_negative:
+                random_choice =  random.choice([0, 1])
+                if random_choice == 1:
+                    move_x_positive = False
+
+                else:
+                    move_x_negative = False
+
+            if move_x_positive:
+                x = car.x + car.length
+                self.update(car, x, y)
+
+            elif move_x_negative:
+                x = car.x - 1
+                self.update(car, x, y)
 
 class Car():
     """
@@ -143,25 +173,28 @@ class Play():
 
     def __init__(self):
         print("Hi! Let's play Rush-Hour!")
-        gridsize = cs50.get_int("What is the gridsize?")
-        csvfile = cs50.get_string("Which CSV file should we use?")
-        correctfile = False
-        while correctfile == False:
-            if (os.path.exists(csvfile)):
-                game = Game(csvfile, gridsize)
-                correctfile = True
-            else:
-                csvfile = cs50.get_string("CSV file does not exist. Which CSV file should we use?")
-        print("OK, let's go!")
+        gridsize = 6
+        csvfile = "Rushhour6x6_1.csv"
+        # gridsize = cs50.get_int("What is the gridsize?")
+        # csvfile = cs50.get_string("Which CSV file should we use?")
+        # correctfile = False
+        # while correctfile == False:
+        #     if (os.path.exists(csvfile)):
+        #         game = Game(csvfile, gridsize)
+        #         correctfile = True
+        #     else:
+        #         csvfile = cs50.get_string("CSV file does not exist. Which CSV file should we use?")
+        # print("OK, let's go!")
 
         # dit moet aangepast worden
+        game = Game(csvfile, gridsize)
         moves = 0
         gamewon = False
         while gamewon == False:
-            game.move()
-            frame(game)
+            game.random_move()
             gamewon = game.win()
             moves += 1
+            print(moves)
         print(f"Done! It took {moves} moves to win the game")
 
 if __name__ == "__main__":
