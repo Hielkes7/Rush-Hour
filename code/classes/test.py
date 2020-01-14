30d19cad78ad4321c93032bd37648cd029cf1dbb
Hiele Wilkes <hiele21@hotmail.com>
2020-01-14 13:31:15 +0100

    some change

--- code/classes/test.py
+++ code/classes/test.py
@@ -6,5 +6,17 @@ cars2 = {'A': [1, 5, 'H', '2'],
         'B': [3, 5, 'H', '2'],
         'C': [1, 4, 'H', '2']}
 
-if cars == cars2:
-    print("yaaa")
+for i in range(10):
+    file_name = "frame" + str(i) + ".png"
+    print(file_name)
+
+
+list = [0, 1, 2, 3]
+
+
+def function(x):
+    return x + 1
+
+x = 5
+x = function(x)
+print(x)
