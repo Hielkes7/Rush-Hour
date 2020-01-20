6e08f75db55674753d62c80a96a2aa04bc7c5e4e
Hiele Wilkes <hiele21@hotmail.com>
2020-01-20 17:24:05 +0100

    added 'make_path_free' algorithm/wincondition

--- code/classes/algorithms.py
+++ code/classes/algorithms.py
@@ -34,7 +34,7 @@ def update(game, car, x, y):
             game.grid[x][y] = car.id
             x += 1
 
-def redcar_path_free(game):
+def check_path_free(game):
     """
         Checks if path of the red car is free and updates it.
     """
@@ -47,6 +47,7 @@ def redcar_path_free(game):
         if game.grid[x_path][y_path] != 0:
             return False
 
+    # move redcar to exit
     x = game.gridsize - 1
     y = game.gridexit
     update(game, redcar, x, y)
@@ -411,11 +412,95 @@ def queue_algorithm_hiele(game):
     # back to the path
     return False
 
+def make_path_free(game):
+    """
+        This algorithm checks the path can be freed
+    """
+    cars_in_path = []
 
+    # look for cars in the exit path of the red car, only look to the right
+    for x in range(game.redcar.x + game.redcar.length, game.gridsize + 1):
+         spot = game.grid[x][game.gridexit]
+         if spot != 0:
+             for car in game.cars:
+                 if car.id == spot:
+                     cars_in_path.append(car)
+                     break
 
+    # check if cars are able to completely move off the path
+    cars_movable = []
+    for car in cars_in_path:
 
+        # all cars on the path are vertical orientated
+        move_y_positive = movable_up(game, car)
+        move_y_negative = movable_down(game, car)
 
+        # if the car is able to move up or down and completely of the path:
+        move_up = False
+        move_down = False
 
+        # check how far the car can go up
+        if move_y_positive:
+            new_y_up = car.y
+
+            # keep checking the spot above the previous spot to see if it's 0
+            # do this untill hitting a car or the upper edge
+            while new_y_up + car.length < 6:
+                if game.grid[car.x][new_y_up + car.length] == 0:
+                    new_y_up += 1
+                else:
+                    break
+
+            # check if the new position is completely off the path
+            if new_y_up > game.gridexit:
+                move_up = True
+            else:
+                move_up = False
+
+        # check how far the car can go down
+        if move_y_negative:
+            new_y_down = car.y
+
+            # keep checking the spot below the previous spot to see if it's 0
+            # do this untill hitting a car or the lower edge
+            while new_y_down > 0:
+                if game.grid[car.x][new_y_down - 1] == 0:
+                    new_y_down -= 1
+                else:
+                    break
+
+            # check if the new position is completely off the path
+            if new_y_down + car.length - 1 < game.gridexit:
+                move_down = True
+            else:
+                move_down = False
+
+        if move_down and move_up:
+            random_choice = random.choice([0, 1])
+            if random_choice == 1:
+                move_down = False
+            else:
+                move_up = False
+
+        if move_down:
+            cars_movable.append([car, new_y_down])
+
+        elif move_up:
+            cars_movable.append([car, new_y_up])
+
+    # check if the path will be completely free when all the movable cars are moved
+    if len(cars_in_path) == len(cars_movable):
+
+        # move all movable cars
+        for [car, new_y] in cars_movable:
+            update(game, car, car.x, new_y)
+
+        # move red car to exit
+        x = game.gridsize - 1
+        y = game.gridexit
+        update(game, game.redcar, x, y)
+        return True
+    return False
 
 def queue_algorithm(game):
     """
