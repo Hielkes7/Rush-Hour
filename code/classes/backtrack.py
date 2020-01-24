532529943b8aab2beadd068e3bc6bc4fa13ccc25
Liz Mooij <lizmooij@wcw-stud-145-109-37-36.wireless.uva.nl>
2020-01-24 14:02:04 +0100

    still one bug, but almost finished

--- code/classes/backtrack.py
+++ code/classes/backtrack.py
@@ -6,7 +6,7 @@ class Backtrack():
         This class represents an algorithm that uses function "add final grids" to
         find final grids or grids that are a variable steps before final grids
         and uses function "random moves backtrack" to randomly move and check if the
-        current grid is known in the grid dictionary thus it known how many moves until 
+        current grid is known in the grid dictionary thus it known how many moves until
         the end.
     """
 
@@ -35,7 +35,7 @@ class Backtrack():
             while not gamewon:
 
                 # executes a random step and returns a list with info about the step
-                current_move = algorithms.random_max_step_non_recurrent(game)
+                current_move = algorithms.random_max_step_non_recurring(game)
 
                 # adds the move to the list with all game moves
                 game_moves.append(current_move)
@@ -52,7 +52,7 @@ class Backtrack():
 
             for i in range(amount_of_steps):
                 move_list += game_moves[list_length - i]
-                if game_grid[list_length - i] in self.grid_dictionary.keys():
+                if game_grids[list_length - i] in self.grid_dictionary.keys():
                     # kijk of de values minder zijn
                     if len(self.grid_dictionary[game_grid[list_length - i]]) > (i + 1):
                         self.grid_dictionary[game_grid[list_length - i]] = move_list
@@ -61,7 +61,7 @@ class Backtrack():
                     self.grid_dictionary[game_grid[list_length - i]] = move_list
                     # voeg er nog aan toe dat alle moves worden toegevoegd
 
-    def random_moves_backtrack(self, amount_of_moves):
+    def random_moves_backtrack(self, amount_of_games):
         """
             Moves random and checks if current grid is already known.
         """
@@ -72,5 +72,21 @@ class Backtrack():
             while not gamewon:
                 algorithms.random_max_step_non_recurring(game)
                 if game.grid in self.grid_dictionary.keys():
-                        pass
-                gamewon = algorithms.win(game)
+                        moves = self.grid_dictionary[game.grid]
+                        for move in moves:
+                            car = move[0]
+                            x = move[1]
+                            y = move[2]
+                            algorithms.update(game, car, x, y)
+                            gamewon = True
+                            print("endstate found!")
+                else:
+                    algorithms.check_path_free(game)
+                    gamewon = algorithms.win(game)
+            print(f"done! Game was won in {game.moves} moves")
+
+
+if __name__ == "__main__":
+    backtrack = Backtrack()
+    backtrack.add_final_grids(10, 5)
+    backtrack.random_moves_backtrack(10)
